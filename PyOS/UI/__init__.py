8dcc6f3824d550456da6f427390deaf1d241f7f1
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-05-10 11:55:14 -0400

    Add files via upload

---
+++ PyOS/UI/__init__.py
@@ -0,0 +1,10 @@
+from .widgets import UI;
+from .widgets import SvgImage;
+import PIL as pillow;
+import tkinter.ttk as ttk;
+from PIL import Image;
+from PIL import ImageTk as tkImg;
+
+def print_info(*args):
+    print(*args);
+##end
