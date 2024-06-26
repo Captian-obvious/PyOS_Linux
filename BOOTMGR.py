5689b1f4b45fd55327c7138e0a3657dd24ca86ce
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-06-26 19:23:41 -0400

    Add files via upload

--- BOOTMGR.py
+++ BOOTMGR.py
@@ -1,5 +1,18 @@
+import os,subprocess,time;
 import PyOS.MainExe as main;
-import plymouth_boot as ply;
+if os.path.exists('./splash'):
+    if os.name=="nt":
+        subprocess.run(['splash','--mode=boot']);
+    else:
+        subprocess.run(['./splash','--mode=boot']);
+    ##endif
+else:
+    if os.name=="nt":
+        subprocess.run(['pythonw','plymouth_boot.pyw','--mode=boot']);
+    else:
+        subprocess.run(['./plymouth_boot.pyw','--mode=boot']);
+    ##endif
+##endif
 #EXTERN: load,init;
 if __name__=='__main__':
     main.init();
